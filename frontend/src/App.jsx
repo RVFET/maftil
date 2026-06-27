b3aa19504d72a7941b9033009054d15bf7455027
RVFET <thervfet@gmail.com>
2026-06-27 14:22:07 +0400

    changing workstation from mac to arch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,11 @@
-import { Icon } from "@iconify-icon/solid";
 import { CommandBar } from "./components/CommandBar/CommandBar";
+import { ThemeToggle } from "./components/ThemeToggle/ThemeToggle";
 
 function App() {
   return (
     <>
       <div>
-        <button
-          type="button"
-          aria-label="Toggle dark mode"
-          data-tooltip="Toggle dark mode"
-          data-side="bottom"
-          onclick="document.dispatchEvent(new CustomEvent('basecoat:theme'))"
-          class="btn-icon-outline size-8"
-        >
-          <span class="hidden dark:block">
-            <Icon icon="mdi:white-balance-sunny" height="1em" />
-          </span>
-          <span class="dark:hidden block">
-            <Icon icon="mdi:weather-night" height="1em" />
-          </span>
-        </button>
+        <ThemeToggle />
       </div>
 
       <CommandBar />
---
+++ frontend/src/components/ThemeToggle/ThemeToggle.jsx
@@ -0,0 +1,21 @@
+import { Icon } from "@iconify-icon/solid";
+
+export function ThemeToggle() {
+  return (
+    <button
+      type="button"
+      aria-label="Toggle dark mode"
+      data-tooltip="Toggle dark mode"
+      data-side="bottom"
+      onclick="document.dispatchEvent(new CustomEvent('basecoat:theme'))"
+      class="btn-icon-outline size-8"
+    >
+      <span class="hidden dark:block">
+        <Icon icon="mdi:white-balance-sunny" height="1em" />
+      </span>
+      <span class="dark:hidden block">
+        <Icon icon="mdi:weather-night" height="1em" />
+      </span>
+    </button>
+  );
+}
